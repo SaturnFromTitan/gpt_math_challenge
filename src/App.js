969e9237e8028c6a6dfb8c756d281f168ade460b
Martin Winkel <martin.winkel.pps@gmail.com>
2023-03-30 16:12:59 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
 
 function App() {
+  const [secondsLeft, setSecondsLeft] = useState(60);
+  const [score, setScore] = useState(0);
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [operator, setOperator] = useState("+");
+  const [userAnswer, setUserAnswer] = useState("");
+  const [gameOver, setGameOver] = useState(false);
+
+  const generateNewQuestion = useCallback(() => {
+    setFirstNumber(Math.floor(Math.random() * 10));
+    setSecondNumber(Math.floor(Math.random() * 10) + 1);
+    setNewOperator();
+  }, []);
+
+  useEffect(() => {
+    generateNewQuestion();
+    const timer = setInterval(() => {
+      setSecondsLeft((seconds) => {
+        if (seconds <= 0) {
+          clearInterval(timer);
+          setGameOver(true);
+          return 0;
+        }
+        return seconds - 1;
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [generateNewQuestion]);
+
+  function setNewOperator() {
+    const operators = ["+", "-", "*", "/"];
+    const newOperator = operators[Math.floor(Math.random() * operators.length)];
+
+    if (newOperator === "/") {
+      const product = firstNumber * secondNumber;
+      setFirstNumber(product);
+    }
+
+    setOperator(newOperator);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let correctAnswer;
+    switch (operator) {
+      case "+":
+        correctAnswer = firstNumber + secondNumber;
+        break;
+      case "-":
+        correctAnswer = firstNumber - secondNumber;
+        break;
+      case "*":
+        correctAnswer = firstNumber * secondNumber;
+        break;
+      case "/":
+        correctAnswer = firstNumber / secondNumber;
+        break;
+      default:
+        break;
+    }
+    if (parseFloat(userAnswer) === correctAnswer) {
+      setScore(score + 1);
+    }
+    setUserAnswer("");
+    generateNewQuestion();
+  }
+
+  if (gameOver) {
+    return (
+      <div className="App">
+        <h1>Time's up!</h1>
+        <h2>Your score: {score}</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mental Math Game</h1>
+      <h2>Time remaining: {secondsLeft} seconds</h2>
+      <h3>Score: {score}</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          {firstNumber} {operator} {secondNumber} ={" "}
+          <input
+            type="number"
+            value={userAnswer}
+            onChange={(e) => setUserAnswer(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
